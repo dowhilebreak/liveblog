f2d9e4018961543be20b93b89dbd845e42d31fa2
Jacob Anderson <jacob@jacobanderson.us>
2013-11-17 21:16:37 -0500

    Make single entry template use original entry ID
    In the event of an update, the template was using the current entry ID
    which was making client-side management of updates impossible. Added an
    "original_entry_id" field that is either the ID of the entry that is to
    be replaced or the current ID if no replacement is taking place.

--- classes/class-wpcom-liveblog-entry.php
+++ classes/class-wpcom-liveblog-entry.php
@@ -66,23 +66,25 @@ class WPCOM_Liveblog_Entry {
 	}
 
 	public function get_fields_for_render() {
-		$entry_id     = $this->comment->comment_ID;
-		$post_id      = $this->comment->comment_post_ID;
-		$avatar_size  = apply_filters( 'liveblog_entry_avatar_size', self::default_avatar_size );
+		$entry_id          = $this->comment->comment_ID;
+		$original_entry_id = $this->replaces ? $this->replaces : $entry_id;
+		$post_id           = $this->comment->comment_post_ID;
+		$avatar_size       = apply_filters( 'liveblog_entry_avatar_size', self::default_avatar_size );
 
 		$entry = array(
-			'entry_id'              => $entry_id,
-			'post_id'               => $entry_id,
-			'css_classes'           => comment_class( '', $entry_id, $post_id, false ),
-			'content'               => self::render_content( get_comment_text( $entry_id ), $this->comment ),
-			'original_content'      => get_comment_text( $entry_id ),
-			'avatar_size'           => $avatar_size,
-			'avatar_img'            => get_avatar( $this->comment->comment_author_email, $avatar_size ),
-			'author_link'           => get_comment_author_link( $entry_id ),
-			'entry_date'            => get_comment_date( get_option('date_format'), $entry_id ),
-			'entry_time'            => get_comment_date( get_option('time_format'), $entry_id ),
-			'timestamp'             => $this->get_timestamp(),
-			'is_liveblog_editable'  => WPCOM_Liveblog::is_liveblog_editable(),
+			'entry_id'             => $entry_id,
+			'original_entry_id'    => $original_entry_id,
+			'post_id'              => $post_id,
+			'css_classes'          => comment_class( '', $entry_id, $post_id, false ),
+			'content'              => self::render_content( get_comment_text( $entry_id ), $this->comment ),
+			'original_content'     => get_comment_text( $entry_id ),
+			'avatar_size'          => $avatar_size,
+			'avatar_img'           => get_avatar( $this->comment->comment_author_email, $avatar_size ),
+			'author_link'          => get_comment_author_link( $entry_id ),
+			'entry_date'           => get_comment_date( get_option('date_format'), $entry_id ),
+			'entry_time'           => get_comment_date( get_option('time_format'), $entry_id ),
+			'timestamp'            => $this->get_timestamp(),
+			'is_liveblog_editable' => WPCOM_Liveblog::is_liveblog_editable(),
 		);
 
 		return $entry;
--- templates/liveblog-single-entry.php
+++ templates/liveblog-single-entry.php
@@ -1,8 +1,8 @@
-<div id="liveblog-entry-<?php echo $entry_id; ?>" <?php echo $css_classes; ?> data-timestamp="<?php echo $timestamp; ?>">
+<div id="liveblog-entry-<?php echo $original_entry_id; ?>" <?php echo $css_classes; ?> data-timestamp="<?php echo $timestamp; ?>">
 	<header class="liveblog-meta">
 		<span class="liveblog-author-avatar"><?php echo $avatar_img; ?></span>
 		<span class="liveblog-author-name"><?php echo $author_link; ?></span>
-		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo $entry_id; ?>"><span class="date"><?php echo $entry_date; ?></span><span class="time"><?php echo $entry_time; ?></span></a></span>
+		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo $original_entry_id; ?>"><span class="date"><?php echo $entry_date; ?></span><span class="time"><?php echo $entry_time; ?></span></a></span>
 	</header>
 	<div class="liveblog-entry-text" data-original-content="<?php echo esc_attr( $original_content ); ?>">
 		<?php echo $content; ?>
